304b9516fd6322a0fed89e36e80dd7632103feb9
Júlio Santos <hi@whoisjuliosantos.com>
2018-05-29 11:13:52 +0100

    Improve contract legibility

--- truffle/tests/recorder.js
+++ truffle/tests/recorder.js
@@ -1,28 +1,28 @@
 var Recorder = artifacts.require("Recorder");
-contract('Recorder', function(accounts) {
-  it("records event", function(done) {
-    Recorder.deployed().then(function (instance) {
+contract('Recorder', (accounts) => {
+  it("records event", (done) => {
+    Recorder.deployed().then((instance) => (
       instance
         .Record({}, {fromBlock: 0, toBlock: "latest"})
-        .get(function (error, events) {
+        .get((error, events) => {
           assert.equal(
             0,
             events.length,
             "There should be no events at start."
           );
-          instance.record("pokemon").then(function () {
+          instance.record("pokemon").then(() => (
             instance
               .Record({}, {fromBlock: 0, toBlock: "latest"})
-              .get(function (error, events) {
+              .get((error, events) => {
                 assert.equal(
                   1,
                   events.length,
                   "There should be one event after recording."
                 );
                 done();
-              });
-          });
-        });
-    });
+              })
+          ));
+        })
+    ));
   });
 });
